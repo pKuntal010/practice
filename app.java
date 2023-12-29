a58e31f78ef32a87076acd1031a7e9ccd986a4d4
Kuntal <kuntal.paul.official@gmail.com>
2023-12-30 00:13:24 +0530

    task 101 completed

--- app.java
+++ app.java
@@ -4,7 +4,12 @@ public static void main(String[] args){
 ......
 }
 
+
 //102
 public void show(){
 	System.out.println("I am inside show");
+
+//101
+public void display(){
+	System.out.println("I am inside display");
 }
